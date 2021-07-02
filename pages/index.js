022329cf4300636747029ef51cbaab79ecde7920
isaacrdz <zack_cpp@hotmail.com>
2021-07-01 22:18:24 -0500

    s

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,40 @@ import React from "react";
 
 import { Container, Typography } from "@material-ui/core";
 
-const marca = ({ marca }) => {
+const marca = ({ vehicles }) => {
   return (
     <Container>
-      <Typography>{marca}</Typography>
+      {vehicles &&
+        vehicles.map((vehicle, i) => (
+          <Typography key={i}>{vehicle.name} </Typography>
+        ))}
     </Container>
   );
 };
 
 export const getServerSideProps = async (context) => {
-  const { req, query, asPath, pathname } = context;
+  const { req } = context;
 
   const url = req.headers.host;
 
-  const store = url.split(".")[0];
+  const storeP = url.split(".")[0];
+
+  const resStore = await fetch(`https://apicarone.com/api/v1/stores/${storeP}`);
+
+  const store = await resStore.json();
+
+  const { data } = store;
+
+  const res = await fetch(
+    `https://apicarone.com/api/v1/vehicles?make=${data.make._id}`
+  );
+
+  const vehicles = await res.json();
 
   return {
     props: {
-      marca: store,
+      vehicles: vehicles.data,
+      // marca: context.params.marca,
     },
   };
 };
