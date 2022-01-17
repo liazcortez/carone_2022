d71ef3961b030cb17849bb8577f6f8be33857e1a
MadMofin <Arthurjaziel1@live.com>
2022-01-17 17:20:29 -0600

    s

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,14 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 // import Divider from "@material-ui/core/Divider";
-// import Nav from "../components/Nav";
-// import Tabs from "../components/Tabs";
+import Tabs from "../components/Tabs";
 
-// import Banner from "../components/Banner";
+import Banner from "../components/Banner";
 // import Carousel from "../components/Carousel";
-// import CarCardList from "../components/CarCardList";
-// import Meta from "../components/Meta";
+import CarCardList from "../components/CarCardList";
+import Meta from "../components/Meta";
 
-// const imageUrl = "https://carone.com.mx/wp-content/uploads/2021/03/banner2.jpg";
+const imageUrl = "https://carone.com.mx/wp-content/uploads/2021/03/banner2.jpg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,35 +27,28 @@ const Home = ({ makes, categories }) => {
 
   return (
     <>
-      {/* <Meta title="Car One Group" description="Autos Nuevo, Seminuevos" /> */}
+      <Meta title="Car One Group" description="Autos Nuevo, Seminuevos" />
       <Container maxWidth="lg">
-        {/* <Banner imageUrl={imageUrl} /> */}
-        {/* <Tabs categories={categories} /> */}
-        {/* <CarCardList title="BUSCAR POR MARCA" data={makes} /> */}
+        <Banner imageUrl={imageUrl} />
+        <Tabs categories={categories} />
+        <CarCardList title="BUSCAR POR MARCA" data={makes} />
       </Container>
     </>
   );
 };
 
 export const getStaticProps = async (context) => {
-  // const res = await fetch(`https://apicarone.com/api/v1/makes`);
-  // const makes = await res.json();
+  const res = await fetch(`https://apicarone.com/api/v1/makes`);
+  const makes = await res.json();
 
-  // const resCategories = await fetch(`https://apicarone.com/api/v1/categories`);
-  // const categories = await resCategories.json();
-
-  // return {
-  //   props: {
-  //     makes: makes.data,
-  //     categories: categories.data,
-  //   },
-  // };
+  const resCategories = await fetch(`https://apicarone.com/api/v1/categories`);
+  const categories = await resCategories.json();
 
   return {
     props: {
-      makes: [],
-      categories: []
-    }
-  }
+      makes: makes.data,
+      categories: categories.data,
+    },
+  };
 };
 export default Home;
