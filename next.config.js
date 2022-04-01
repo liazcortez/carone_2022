1248c1552a0f0a9dc0d101c772597db362f17e8a
isaacrdz <zack_cpp@hotmail.com>
2022-04-01 12:41:29 -0500

    s

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
+  webpack5: false,
   reactStrictMode: true,
   images: {
     domains: ["carone.com.mx", "automotive-api.s3.us-east-2.amazonaws.com"],
-    webpack5: false,
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
