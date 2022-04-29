b0a356a1be79fd945bca6fe2de8c3cdab2fe31f5
liazcortez <40672433+liazcortez@users.noreply.github.com>
2022-04-29 17:32:36 -0500

    fix

--- pages/autos/index.js
+++ pages/autos/index.js
@@ -6,7 +6,7 @@ import Meta from "../../components/Meta";
 import Pagination from "../../components/Pagination";
 import useVehicles from "../../hooks/useVehicle";
 import Banner from "../../components/Banner";
-
+import { baseURL } from "../../api/api";
 
 const Index = ({ vehiclesSP, total, makes, categories }) => {
 
@@ -31,7 +31,36 @@ const Index = ({ vehiclesSP, total, makes, categories }) => {
 
   return (
     <>
-     
+      <Meta title="Busca tu Auto - Car One Group" description="Busca tu Auto Nuevo"/>
+
+<Container maxWidth="lg">
+  <SearchBar
+    setQuery={setQuery}
+    query={query}
+    makes={makes}
+    categories={categories}
+    setCategory={setCategory}
+    setMake={setMake}
+    category={category}
+    make={make}
+    disableTopBar={disableTopBar}
+    setPage={setPage}
+    sort={sort}
+    setSort={setSort}
+  />
+  <Divider style={{ marginBottom: "50px" }} />
+  {vehicles ? (
+    <CarList vehicles={vehicles} loading={loading} />
+  ) : (
+    <CarList vehicles={vehiclesSP} loading={loading} />
+  )}
+  <Pagination
+    total={results !== null ? results : total}
+    page={page}
+    limit={12}
+    changePage={changePage}
+  />
+</Container>
     </>
   );
 };
