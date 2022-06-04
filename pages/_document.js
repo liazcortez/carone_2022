855bcbfbc3c347015b51d7970baa9693c91603ab
isaacrdz <zack_cpp@hotmail.com>
2022-06-04 07:47:15 -0500

    s

--- pages/_document.js
+++ pages/_document.js
@@ -2,12 +2,25 @@ import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
 import theme from "../src/theme";
+import Script from "next/script";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
+          {/* Segment */}
+
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+           !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="OLUS7C9S9HqyQVXqvGeGuUycLgYBA4Yp";;analytics.SNIPPET_VERSION="4.15.3";
+           analytics.load("${process.env.NEXT_PUBLIC_SEGMENT}");
+           analytics.page();
+           }}();
+        `}
+          </Script>
+
+          {/* end Segment */}
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
 
@@ -25,13 +38,11 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
           <script src="https://www.youtube.com/iframe_api"></script>
-
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-
       </Html>
     );
   }
