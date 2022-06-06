d33f0d133eb91c720dea67323ecc92acfe7d9d8e
MadMofin <Arthurjaziel1@live.com>
2022-06-06 16:26:01 -0500

    s

--- pages/estimacion/index.js
+++ pages/estimacion/index.js
@@ -222,7 +222,7 @@ const CalcPrice = () => {
                         vehicle.price && (
                         <Typography variant="h6" gutterBottom style={{ fontSize: 17 }}>
                             Precio de Toma &nbsp;
-                            <NumberFormat value={((vehicle.price * 0.8) * 0.8).toFixed(0)} displayType={"text"} thousandSeparator={true} prefix={"$"}/>
+                            <NumberFormat value={( ( prices.promedio - (vehicle.price * 0.2) ) * 0.8).toFixed(0)} displayType={"text"} thousandSeparator={true} prefix={"$"}/>
                         </Typography>
                         )
                         }
