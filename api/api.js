258b427ae4c7c157f5ee688e86c72982ec5489ae
liazcortez <40672433+liazcortez@users.noreply.github.com>
2022-06-06 12:53:40 -0500

    u meta info

--- api/api.js
+++ api/api.js
@@ -5,8 +5,8 @@ if (process.env.NODE_ENV === "production") {
   baseURL = "https://apicarone.com/api/v1";
 } else {
 
-  // baseURL = "https://apicarone.com/api/v1";
-  baseURL = "http://localhost:5000/api/v1";
+  baseURL = "https://apicarone.com/api/v1";
+  // baseURL = "http://localhost:5000/api/v1";
 }
 export { baseURL };
 
