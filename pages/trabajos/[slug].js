dc0f568347a93e9a270f770d102abe08de65873c
liazcortez <40672433+liazcortez@users.noreply.github.com>
2022-06-13 14:46:56 -0500

    mini fix

--- pages/trabajos/[slug].js
+++ pages/trabajos/[slug].js
@@ -17,6 +17,7 @@ import BreadCrumJobs from "../../components/BreadCrumJobs";
 import GeneralModal from "../../components/GeneralModal";
 import useAuth from "../../hooks/useAuth";
 import LocalAtmIcon from '@material-ui/icons/LocalAtm';
+import ObjExist from '../../utils/ObjExist';
 const slug = ({ job }) => { 
 
   moment.locale("es-mx");
@@ -64,7 +65,8 @@ const slug = ({ job }) => {
                       variant="subtitle2"
                       style={{ textTransform: "capitalize" }}
                     >
-                      {job.store.make.name} {job.store.name}
+                     {ObjExist(job,['store','make','name'])} {ObjExist(job,['store','name'])}
+
                     </Typography>
                   </Box>
                   <Box>
---
+++ utils/ObjExist.js
@@ -0,0 +1,27 @@
+const ObjExist = (obj, keys = [],defaultReturn='') => {
+
+	
+
+	let value = defaultReturn;
+
+	if (!obj) return "";
+
+	var currentObj = obj;
+
+	for (let index = 0; index < keys.length; index++) {
+
+	  const current = keys[index];
+	  
+	  if (typeof currentObj[current] == "boolean")return currentObj[current]?1:0;
+
+	  if (!currentObj[current]) return defaultReturn;
+
+	  value = (index, currentObj[current]);
+	  currentObj = currentObj[current];
+
+	}
+	
+	return value;
+};
+
+export default ObjExist;
