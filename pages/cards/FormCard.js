e5bf9cab7e026a3689c4266d81acd12896491282
Jose Miguel <miguel.hdz.rayon@gmail.com>
2023-01-16 13:34:32 -0600

    s

--- pages/cards/FormCard.js
+++ pages/cards/FormCard.js
@@ -8,17 +8,18 @@ const FormCard = ({card}) => {
         <Grid item xs={12}>
           <Box style={{background:"#000080", borderRadius:"0% 0% 30% 30%", width:"100%"}}>
             <center>
-              <img src={card.stores.make.image} alt={card?.store?.make?.name} style={{marginBottom:10, width:"30%"}}/>
+              <img src={card?.stores?.make?.image} alt={card?.store?.make?.name} style={{marginBottom:10, width:"30%"}}/>
             </center>
           </Box>
           <Box style={{width:"100%", marginBottom:10}}>
             <center>
-              <Avatar src={card.image} alt={card.slug}  sx={{ width: 160, height: 160 }}/>
+              <Avatar src={card?.image} alt={card?.slug}  sx={{ width: 160, height: 160 }}/>
             </center>
           </Box>
-          <Box>
+          <Box style={{width:"100%", marginBottom:10}}>
             <center>
-              <Typography></Typography>
+              <Typography variant="h5" >{card?.name}</Typography>
+              <Typography variant="caption" >{card?.job}</Typography>
             </center>
           </Box>
         </Grid>
