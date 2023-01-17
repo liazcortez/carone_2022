4bdfaaceb7a51975ff3bb5fb4b30e4b63e56aa6f
Jose Miguel <miguel.hdz.rayon@gmail.com>
2023-01-17 10:29:18 -0600

    s

--- pages/cards/FormCard.js
+++ pages/cards/FormCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography, Grid, Avatar, Divider} from "@mui/material";
+import { Box, Typography, Grid, Avatar, Divider, Stack, Link} from "@mui/material";
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import YouTubeIcon from '@mui/icons-material/YouTube';
@@ -10,14 +10,14 @@ import { Capitalize } from '../../utils/capitalize';
 import { capitalCase } from 'change-case';
 
 const FormCard = ({card}) => {
-  console.log(card)
+  const prettyPhone = (p) => `+52 ${p[0]}${p[1]} ${p[2]}${p[3]}${p[4]}${p[5]} ${p[6]}${p[7]}${p[8]}${p[9]}`
   return (
     <Grid container spacing={2}>
         <Grid item xs={12}>
           <Box style={{ borderRadius:"0% 0% 30% 30%", width:"100%", marginBottom:10}}>
             <center>
-              <img src={card?.stores?.make?.image} alt={card?.store?.make?.name.toUpperCase()} style={{width:"40%", marginTop:5}}/>
-              <Typography variant='h6' fontSize={"21px"} style={{marginBottom:12, fontFamily:"tahoma"}}>{card?.stores?.make?.name.toUpperCase()} {Capitalize(card?.stores?.name)}</Typography>
+              <img src={card?.stores?.make?.image} alt={card?.store?.make?.name.toUpperCase()} style={{width:"42%", marginTop:5}}/>
+              <Typography variant='h6' fontSize={35} style={{marginBottom:12, fontWeight: 700}}>{card?.stores?.make?.name.toUpperCase()} {Capitalize(card?.stores?.name)}</Typography>
             </center>
           </Box>
           <Divider sx={{ marginTop: 2, marginBottom: 2, width:"0%"}} />
@@ -26,52 +26,66 @@ const FormCard = ({card}) => {
               <Avatar src={card?.image} alt={card?.slug}  sx={{ width: 100, height: 100 }}/>
             </center>
           </Box>
+          <Divider sx={{ marginTop: 2, marginBottom: 2, width:"0%"}} />
           <Box style={{width:"100%", marginBottom:16, marginTop:10}}>
             <center>
-              <Typography variant="h6" style={{fontFamily:"Segoe UI"}} >{capitalCase(card?.name)}</Typography>
-              <Typography variant="body" style={{fontFamily:"Segoe UI"}}>{Capitalize(card?.job)}</Typography>
-              <Typography variant='body2' style={{fontFamily:"Segoe UI"}} ><b>{card?.email}</b></Typography>
+              <Typography variant="h6" style={{fontFamily:"tahoma"}} >{capitalCase(card?.name)}</Typography>
+              <Typography variant="caption" color="grayText" style={{fontFamily:"tahoma", fontSize:"14px"}}>{Capitalize(card?.job)}</Typography>
+              <Typography variant='body2' style={{fontFamily:"tahoma"}} >
+                <b>
+                  <a
+                    href={`mailto:${card?.email}`}
+                    style={{ textDecoration: "none", color: "#000000" }}
+                  >
+                    {card?.email}
+                  </a>
+                </b>
+              </Typography>
+              <Typography variant='body2' style={{fontFamily:"tahoma"}} >
+                  <a
+                    href={`tel:+52${card?.phone}`}
+                    style={{ textDecoration: "none", color: "#000000" }}
+                  >
+                    {prettyPhone(card?.phone)}
+                  </a>
+              </Typography>
             </center>
           </Box>
           <center>
-          <Divider sx={{ marginTop: 3, marginBottom: 6, width:"80%"}} />
+          <Divider sx={{ marginTop: 3, marginBottom: 8, width:"80%"}} />
           </center>
           <Box style={{width:"100%"}}>
             <center>
-              <Box style={{display:"flex", justifyContent:"space-between", marginBottom:8}}>
-                <Box style={{display:"flex", width:"100%", alignItems:"center"}}>
-                  <FacebookIcon style={{color:"#000080", marginLeft:2, marginRight:3, fontSize:"30px"}}/>
-                  <Typography variant='caption' style={{fontSize:"12px", fontFamily:"Arial"}}><em>{card?.stores?.make?.name.toUpperCase()} {card?.stores?.name}</em></Typography>
-                </Box>
-                <Box style={{display:"flex", width:"100%", alignItems:"center"}}>
-                  <TwitterIcon style={{color:"#87CEEB", marginLeft:2, marginRight:3, fontSize:"30px"}}/>  
-                  <Typography variant='caption' style={{fontSize:"12px", fontFamily:"Arial"}}><em>@{card?.stores?.make?.name.toUpperCase()}_{card?.stores?.name}</em></Typography>
-                </Box>
-              </Box>
-              <Box style={{display:"flex", justifyContent:"space-between", marginBottom:8}}>
-                <Box style={{display:"flex", width:"100%", alignItems:"center"}}>
-                  <YouTubeIcon style={{color:"#ff0000", marginLeft:2, marginRight:3, fontSize:"30px"}}/>
-                  <Typography variant='caption' style={{fontSize:"12px", fontFamily:"Arial"}}><em>{card?.stores?.make?.name.toUpperCase()} {card?.stores?.name}</em></Typography>
-                </Box>
-                <Box style={{display:"flex", width:"100%", alignItems:"center"}}>
-                  <InstagramIcon style={{color:"#ff0080", marginLeft:2, marginRight:3, fontSize:"30px"}}/>
-                  <Typography variant='caption' style={{fontSize:"12px", fontFamily:"Arial"}}><em>{card?.stores?.make?.name.toUpperCase()} {card?.stores?.name}</em></Typography>
-                </Box>
-              </Box>
-              <Box style={{display:"flex", justifyContent:"space-between", marginBottom:8}}>
-                <Box style={{display:"flex", width:"100%", alignItems:"center"}}>
-                  <MusicNoteIcon style={{ marginLeft:2, marginRight:3, fontSize:"30px"}}/>
-                  <Typography variant='caption' style={{fontSize:"12px", fontFamily:"Arial"}}><em>{card?.stores?.make?.name.toUpperCase()} {card?.stores?.name}</em></Typography>
-                </Box>
-                <Box style={{display:"flex", width:"100%", alignItems:"center"}}>
-                  <WhatsAppIcon style={{color:"#25D366", marginLeft:2, marginRight:3, fontSize:"30px"}}/>
-                  <Typography variant='caption' style={{fontSize:"12px", fontFamily:"Arial"}}><em>{card?.stores?.dpxPhone}</em></Typography>
-                </Box>
-              </Box>
+              <Stack spacing={2} direction="row" style={{justifyContent:"center"}}>
+                {
+                  card && card.facebook !== '' &&
+                  <Link href={card?.facebook} underline="none" target="_blank" rel="noopener noreferrer">
+                    <FacebookIcon style={{color:"#000080", fontSize:"30px"}}/>
+                  </Link>
+                }
+                {
+                  card && card.twitter !== '' &&
+                  <Link href={card?.twitter} underline="none" target="_blank" rel="noopener noreferrer">
+                    <TwitterIcon style={{color:"#87CEEB",  fontSize:"30px"}}/> 
+                  </Link>
+                } 
+                <Link href={card?.youtube} underline="none" target="_blank" rel="noopener noreferrer">
+                  <YouTubeIcon style={{color:"#ff0000",  fontSize:"30px"}}/>
+                </Link>
+                <Link href={card?.instagram} underline="none" target="_blank" rel="noopener noreferrer">
+                  <InstagramIcon style={{color:"#ff0080",  fontSize:"30px"}}/>
+                </Link>
+                <Link href={card?.tiktok} underline="none" target="_blank" rel="noopener noreferrer">
+                  <MusicNoteIcon style={{  fontSize:"30px", color:"#000000"}}/>
+                </Link>
+                <Link href={card?.wsp} underline="none" target="_blank" rel="noopener noreferrer">
+                  <WhatsAppIcon style={{color:"#25D366", fontSize:"30px"}}/>
+                </Link>
+              </Stack>
             </center>
           </Box>
-          <Divider sx={{ marginTop: 5.4, marginBottom: 2, width:"0%"}} />
-          <Box style={{background:"#000080", borderRadius:"60% 10% 0% 0%", width:"100%", marginTop:20, padding:0}}>
+          <Divider sx={{ marginTop: 5.4, width:"0%"}} />
+          <Box style={{background:"#1278E0", borderRadius:"60% 10% 0% 0%", width:"100%", marginTop:20, padding:0}}>
             <center>
               <Box style={{display:"flex", justifyContent:"center", alignItems:"center", paddingTop:"16px", paddingBottom:"16px"}}>
                 <Box style={{width:"50%"}}>
